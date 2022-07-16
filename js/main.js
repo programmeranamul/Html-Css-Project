4b2a002704f4f1bfa8675d3f380f6f559c70aa7e
programmeranamul <programmer.anamul@gmail.com>
2022-07-16 18:02:40 +0600

    Project Complete

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const navLinks = document.querySelector(".nav-links");
+const openMenu = () => {
+  navLinks.style.display = "flex";
+};
+
+const closeMenu = () => {
+  navLinks.style.display = "none";
+};
